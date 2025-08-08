b885b1af606cbe62a2eaf043b43af318ff9d314a
ARUN B S <103768663+arunkalayam@users.noreply.github.com>
2025-08-08 20:38:31 +0530

    24081

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import settings
+
+def homepage(request):
+    context={
+        'restaurant_name': settings.RESTUARANT_NAME
+        }
+        return render(request,'home.html',context)
